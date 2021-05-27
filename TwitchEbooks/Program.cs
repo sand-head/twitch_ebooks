9fc7dd6cf099937bf647f6dba35d1475ba7ec1e7
Justin Schweigert <justin@schweigert.dev>
2021-05-26 21:28:31 -0500

    Send ASP.NET logs thru Serilog, un-Alpine the Docker image

--- TwitchEbooks/Program.cs
+++ TwitchEbooks/Program.cs
@@ -146,6 +146,7 @@ namespace TwitchEbooks
                     services
                         .AddHostedService<MessageGenerationService>()
                         .AddHostedService<TwitchService>();
-                });
+                })
+                .UseSerilog();
     }
 }
