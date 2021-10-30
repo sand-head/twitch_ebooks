0a99aabb634b07a31774a32e76696b1c6f736cfc
Justin Schweigert <justin@schweigert.dev>
2021-10-30 18:35:57 -0500

    Update to .NET 6, start working on the Twitch auth endpoint

--- TwitchEbooks/Controllers/EventSubController.cs
+++ TwitchEbooks/Controllers/EventSubController.cs
@@ -9,54 +9,53 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using TwitchEbooks.Models.EventSub;
 
-namespace TwitchEbooks.Controllers
+namespace TwitchEbooks.Controllers;
+
+[ApiController, Route("[controller]")]
+public class EventSubController : ControllerBase
 {
-    [ApiController, Route("[controller]")]
-    public class EventSubController : ControllerBase
+    private readonly ILogger<EventSubController> _logger;
+
+    public EventSubController(ILogger<EventSubController> logger)
     {
-        private readonly ILogger<EventSubController> _logger;
+        _logger = logger;
+    }
 
-        public EventSubController(ILogger<EventSubController> logger)
-        {
-            _logger = logger;
-        }
+    [HttpPost]
+    public async Task<IActionResult> ReceiveEventSubPayload([FromHeader(Name = "Twitch-Eventsub-Message-Type")] string messageType)
+    {
+        using var streamReader = new StreamReader(Request.Body);
+        var body = await streamReader.ReadToEndAsync();
+        if (!VerifyEventSubSignature(Request.Headers, body))
+            return StatusCode(403);
 
-        [HttpPost]
-        public async Task<IActionResult> ReceiveEventSubPayload([FromHeader(Name = "Twitch-Eventsub-Message-Type")] string messageType)
-        {
-            using var streamReader = new StreamReader(Request.Body);
-            var body = await streamReader.ReadToEndAsync();
-            if (!VerifyEventSubSignature(Request.Headers, body))
-                return StatusCode(403);
+        _logger.LogInformation("Received EventSub payload of type {PayloadType} from Twitch.", messageType);
+        using var jsonDoc = JsonDocument.Parse(body);
+        if (messageType == "webhook_callback_verification")
+            return Ok(jsonDoc.RootElement.GetProperty("challenge").GetString());
 
-            _logger.LogInformation("Received EventSub payload of type {PayloadType} from Twitch.", messageType);
-            using var jsonDoc = JsonDocument.Parse(body);
-            if (messageType == "webhook_callback_verification")
-                return Ok(jsonDoc.RootElement.GetProperty("challenge").GetString());
+        // put notification on queue for processing
+        var payload = JsonSerializer.Deserialize<EventSubPayload>(body);
+        //await twitchService.EnqueueNotificationAsync(payload);
 
-            // put notification on queue for processing
-            var payload = JsonSerializer.Deserialize<EventSubPayload>(body);
-            //await twitchService.EnqueueNotificationAsync(payload);
+        return Accepted();
+    }
 
-            return Accepted();
-        }
+    private bool VerifyEventSubSignature(IHeaderDictionary headers, string body)
+    {
+        var id = headers["Twitch-Eventsub-Message-Id"];
+        var timestamp = headers["Twitch-Eventsub-Message-Timestamp"];
 
-        private bool VerifyEventSubSignature(IHeaderDictionary headers, string body)
-        {
-            var id = headers["Twitch-Eventsub-Message-Id"];
-            var timestamp = headers["Twitch-Eventsub-Message-Timestamp"];
+        var encoding = new UTF8Encoding();
+        var textBytes = encoding.GetBytes(id + timestamp + body);
+        //var secretBytes = encoding.GetBytes(_transport.Secret);
+        var secretBytes = Array.Empty<byte>();
 
-            var encoding = new UTF8Encoding();
-            var textBytes = encoding.GetBytes(id + timestamp + body);
-            //var secretBytes = encoding.GetBytes(_transport.Secret);
-            var secretBytes = Array.Empty<byte>();
+        using var hash = new HMACSHA256(secretBytes);
+        var hashBytes = hash.ComputeHash(textBytes);
+        var calculatedHash = "sha256=" + BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        var signature = headers["Twitch-Eventsub-Message-Signature"];
 
-            using var hash = new HMACSHA256(secretBytes);
-            var hashBytes = hash.ComputeHash(textBytes);
-            var calculatedHash = "sha256=" + BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
-            var signature = headers["Twitch-Eventsub-Message-Signature"];
-
-            return calculatedHash == signature;
-        }
+        return calculatedHash == signature;
     }
 }
---
+++ TwitchEbooks/Controllers/TwitchAuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using TwitchEbooks.Database;
+using TwitchEbooks.Models;
+
+namespace TwitchEbooks.Controllers;
+
+[ApiController, Route("api/[controller]")]
+public class TwitchAuthController : ControllerBase
+{
+    private readonly TwitchEbooksContext _context;
+    private readonly TwitchSettings _settings;
+    private readonly ILogger<TwitchAuthController> _logger;
+
+    public TwitchAuthController(TwitchEbooksContext context, TwitchSettings settings, ILogger<TwitchAuthController> logger)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+        _settings = settings ?? throw new ArgumentNullException(nameof(settings));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    [HttpGet]
+    public async Task Get()
+    {
+        var scopes = new string[]
+        {
+            "chat:edit",
+            "chat:read"
+        };
+
+        //_context.AccessTokens.Add(await server.RunUntilCompleteAsync());
+        await _context.SaveChangesAsync();
+        _logger.LogInformation("Alrighty, tokens acquired! We'll go ahead and continue starting up now, thanks.");
+    }
+}
